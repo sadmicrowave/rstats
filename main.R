3f1d1af54c2949fb6322be38131d640a3c328679
Corey Farmer <corey.farmer@emerus.com>
2017-07-24 11:11:26 -0500

    updated class functions to incorporate public/private function accessors.  Added count property to class

--- main.R
+++ main.R
@@ -1,6 +1,5 @@
 #!/usr/local/bin/R
 
-
 # --------------------------------------------------------------------------------------
 # Run this script from a command line using the following command:
 #
@@ -10,22 +9,24 @@
 # Source additional modules/classes for usage within main
 source("./bin/modules/Analyze.R")
 
+# ------------------------------ Set Environment Options -------------------------------
+
+#options(error=function()traceback(1))
 # --------------------------------------------------------------------------------------
+
 
 # Declare the name of the file containing historical reference data
 file <- "./res/mm_history.csv"
-
 
 ##############################################################################################
 # ----------------------------------------- MAIN --------------------------------------------#
 ##############################################################################################
 analyze <- Analyze(file)
 
-oneCommon <- analyze$getCommon(analyze$get("data"), "Num1")
-sapply(oneCommon, analyze$normalDistribution, data=analyze$data[["Num1"]])
+#oneCommon <- analyze$common(analyze$get("data"), "Num1")
+#sapply(oneCommon, analyze$normalDistribution, data=analyze$data[["Num1"]])
+#data <- analyze$get("data")
 
+print( analyze$count )
 
-
-
-
-
+#print( pnorm(1:75, mean=mean(data$Num1), sd=sd(data$Num1), lower.tail=FALSE) )

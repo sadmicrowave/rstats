a5f6fd8fefe3ceb30f487a72858c6cca3113c237
Corey Farmer <corey.farmer@emerus.com>
2017-07-25 14:48:11 -0500

    added module imports

--- main.R
+++ main.R
@@ -1,32 +1,39 @@
 #!/usr/local/bin/R
-
-# --------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------- #
 # Run this script from a command line using the following command:
-#
 # $ Rscript main.R
+
+# ------------------------- Set Environment Options ------------------------------------ #
+#options(error=function()traceback(1))
+options(encoding = "UTF-8")
+
+
+# ----------------------------- Install Required Packages ------------------------------ #
 #
-# --------------------------------------------------------------------------------------
+# ------------------------------ Import Libraries/Modules ------------------------------ #
 # Source additional modules/classes for usage within main
 source("./bin/modules/Analyze.R")
-
-# ------------------------------ Set Environment Options -------------------------------
-
-#options(error=function()traceback(1))
-# --------------------------------------------------------------------------------------
+source("./bin/modules/Randomness.R")
 
 
-# Declare the name of the file containing historical reference data
-file <- "./res/mm_history.csv"
+
 
 ##############################################################################################
 # ----------------------------------------- MAIN --------------------------------------------#
 ##############################################################################################
+
+# Declare the name of the file containing historical reference data
+file <- "./res/mm_history.csv"
+
 analyze <- Analyze(file)
+random <- Randomness(analyze$data)
+
+d <- random$wald_wolfowitz(analyze$data[["Num1"]])
+
+print( d )
+#d <- analyze$nonparametric_linear_regression()
 
 #oneCommon <- analyze$common(analyze$get("data"), "Num1")
-#sapply(oneCommon, analyze$normalDistribution, data=analyze$data[["Num1"]])
-#data <- analyze$get("data")
-
-print( analyze$count )
+#sapply(oneCommon, analyze$normal_distribution, data=analyze$data[["Num1"]])
 
 #print( pnorm(1:75, mean=mean(data$Num1), sd=sd(data$Num1), lower.tail=FALSE) )
